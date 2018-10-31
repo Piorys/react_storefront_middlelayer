2b0264ad68de10bb3dfad4da59152b1c62553520
Piotr <piotrryszewski@gmail.com>
2018-10-31 17:41:57 +0100

    CRUD for product done

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -6,7 +6,6 @@ exports.test = function(req, res) {
 
 exports.product_create = (req, res) => {
   let product = new Product({
-    id: req.body.id,
     name: req.body.name,
     price: req.body.price,
     currency: req.body.currency,
@@ -16,8 +15,33 @@ exports.product_create = (req, res) => {
 
   product.save(err => {
     if (err) {
-      return console.error(err);
+      return next(err);
     }
     res.send("Product Created successfully");
+  });
+};
+
+exports.product_details = (req, res) => {
+  Product.findById(req.params.id, (err, product) => {
+    if (err) return next(err);
+    res.send(product);
+  });
+};
+
+exports.product_update = (req, res) => {
+  Product.findByIdAndUpdate(
+    req.params.id,
+    { $set: req.body },
+    (err, product) => {
+      if (err) return next(err);
+      res.send("Product updated");
+    }
+  );
+};
+
+exports.product_delete = (req, res) => {
+  Product.findByIdAndRemove(req.params.id, (err, product) => {
+    if (err) return next(err);
+    res.send("Product deleted");
   });
 };
--- model/product.model.js
+++ model/product.model.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 let productSchema = new Schema({
-  id: { type: Number, required: true },
   name: { type: String, required: true, max: 100 },
   price: { type: String, required: true },
   currency: { type: String, required: true },
